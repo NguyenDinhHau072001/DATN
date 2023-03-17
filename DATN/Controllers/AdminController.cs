e40765b5530ad04d79ea881a01e4593c176a9d9f
nguyen dinh hau <nguyendinhhau0107@gmail.com>
2023-03-17 21:09:19 +0700

    first commit

---
+++ DATN/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DATN.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
